4e7d3b030236759eeb9f29790307ac51535a907c
umme <unnipa01@yahoo.com>
2021-08-19 07:50:43 -0500

    initial copy

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        
+        public bool IsAPalindrome(string word)
+        {
+            string reverse = "";
+            for (int i = word.Length -1;i >=0; i--)
+            {
+                reverse += word[i];                             
+            }
+
+            if (reverse == word)
+                return true;
+            else
+                return false;
+
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,28 @@
 using System;
 using Xunit;
+using System.Collections.Generic;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar",true)]
+        [InlineData("madam",true)]
+        [InlineData("there",false)]
+
+        public void IsAPalindromeTest(string word,bool expected)
         {
+            //Arrange
+            var aTest = new WordSmith();
+
+
+            //Act
+            var actual = aTest.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
 
         }
     }
